cab853aa5da90bc948b783ccfe56bf0dfe5586c7
ziazan <494074947@qq.com>
2021-04-01 22:23:39 +0800

    整理样式文件

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>jashdjsah</h1>
+        <h2>1233213</h2>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
