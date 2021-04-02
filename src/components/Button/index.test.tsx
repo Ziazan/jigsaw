4954ca1519bc7e46b2ad3608cc542b93d2aa42ba
ziazan <494074947@qq.com>
2021-04-02 22:53:01 +0800

    update:添加测试用例

--- src/components/Button/index.test.tsx
+++ src/components/Button/index.test.tsx
@@ -2,8 +2,16 @@ import React from 'react'
 import { render } from '@testing-library/react'
 import Button from './index'
 
-test('first react test case', () => {
-	const wrapper = render(<Button>Nice</Button>)
-	const element = wrapper.queryByText('Nice')
-	expect(element).toBeInTheDocument()
+//分类/分组
+describe('test Button', () => {
+	it('渲染按钮', () => {
+		const wrapper = render(<Button>Nice</Button>)
+		const element = wrapper.queryByText('Nice')
+		expect(element).toBeInTheDocument()
+		expect(element?.tagName).toEqual('BUTTON')
+		expect(element).toHaveClass('btn btn-default')
+	})
+	it('不同属性的按钮', () => {})
+	it('渲染链接', () => {})
+	it('禁止按钮', () => {})
 })
