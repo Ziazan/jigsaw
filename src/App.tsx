8cc550441010a679e30ec8c140091d132bc5b794
ziazan <494074947@qq.com>
2021-04-02 21:55:58 +0800

    update:按钮样式丰富

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,37 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <p>类型</p>
+        <Button
+          size={ButtonSize.Large}
+        > Hello
+        </Button>
         <Button disabled>Hello</Button>
         <Button
           btnType={ButtonType.Primary}
           size={ButtonSize.Large}
         > Hello
         </Button>
+        <Button
+          btnType={ButtonType.Danger}
+          size={ButtonSize.Large}
+        > Hello
+        </Button>
         <Button btnType={ButtonType.Link} href="www.baidu.com">baidu</Button>
+        <p>大小</p>
+        <Button
+          btnType={ButtonType.Primary}
+          size={ButtonSize.Small}
+        > Hello
+        </Button>
+        <Button
+          btnType={ButtonType.Primary}
+          size={ButtonSize.Large}
+        > Hello
+        </Button>
+        <p>Link</p>
+        <Button btnType={ButtonType.Link} href="www.baidu.com">baidu</Button>
+        <Button btnType={ButtonType.Link} disabled={true} href="www.baidu.com">baidu</Button>
       </header>
     </div>
   );
