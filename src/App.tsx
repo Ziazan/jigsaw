15da6e86964a2e427840ea52085fa9d5ca591128
ziazan <494074947@qq.com>
2021-04-02 22:10:20 +0800

    update:添加原生属性

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import Button, { ButtonType, ButtonSize } from './components/Button';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import Button, { ButtonType, ButtonSize } from './components/Button'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>类型</p>
-        <Button
-          size={ButtonSize.Large}
-        > Hello
-        </Button>
-        <Button disabled>Hello</Button>
-        <Button
-          btnType={ButtonType.Primary}
-          size={ButtonSize.Large}
-        > Hello
-        </Button>
-        <Button
-          btnType={ButtonType.Danger}
-          size={ButtonSize.Large}
-        > Hello
-        </Button>
-        <Button btnType={ButtonType.Link} href="www.baidu.com">baidu</Button>
-        <p>大小</p>
-        <Button
-          btnType={ButtonType.Primary}
-          size={ButtonSize.Small}
-        > Hello
-        </Button>
-        <Button
-          btnType={ButtonType.Primary}
-          size={ButtonSize.Large}
-        > Hello
-        </Button>
-        <p>Link</p>
-        <Button btnType={ButtonType.Link} href="www.baidu.com">baidu</Button>
-        <Button btnType={ButtonType.Link} disabled={true} href="www.baidu.com">baidu</Button>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<p>类型</p>
+				<Button
+					className="my-btn"
+					onClick={(e) => {
+						e.preventDefault
+						alert(123)
+					}}
+					size={ButtonSize.Large}
+				>
+					Hello
+				</Button>
+				<Button disabled>Hello</Button>
+				<Button btnType={ButtonType.Primary} size={ButtonSize.Large}>
+					Hello
+				</Button>
+				<Button btnType={ButtonType.Danger} size={ButtonSize.Small}>
+					Hello
+				</Button>
+				<Button btnType={ButtonType.Link} href="www.baidu.com">
+					baidu
+				</Button>
+				<p>大小</p>
+				<Button btnType={ButtonType.Primary} size={ButtonSize.Small}>
+					Hello
+				</Button>
+				<Button btnType={ButtonType.Primary} size={ButtonSize.Large}>
+					Hello
+				</Button>
+				<p>Link</p>
+				<Button btnType={ButtonType.Link} href="www.baidu.com">
+					baidu
+				</Button>
+				<Button btnType={ButtonType.Link} disabled={true} href="www.baidu.com">
+					baidu
+				</Button>
+			</header>
+		</div>
+	)
 }
 
-export default App;
+export default App
