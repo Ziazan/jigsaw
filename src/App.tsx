b9e3133aa3e75555393ebb9e418728115704900e
ziazan <494074947@qq.com>
2021-04-07 23:30:36 +0800

    transition eg

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,66 @@
-import React from 'react'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-library.add(fas)
+import React, { useState } from 'react';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+library.add(fas);
 
-import Button, { ButtonType, ButtonSize } from './components/Button'
-import Menu from './components/Menu'
-import MenuItem from './components/Menu/menuItem'
-import SubMenu from './components/Menu/SubMenu'
-import Icon from './components/Icon'
+import Button, { ButtonType, ButtonSize } from './components/Button';
+import Menu from './components/Menu';
+import MenuItem from './components/Menu/menuItem';
+import SubMenu from './components/Menu/SubMenu';
+import Icon from './components/Icon';
+import Transition from './components/Transition';
 
-import './App.css'
+import './App.css';
 
 function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<h1>图标</h1>
-				<Icon icon="coffee" theme="danger" size="5x" />
-				<Icon icon="arrow-down" theme="danger" size="5x" />
-				<h1>menu</h1>
-				<p>水平</p>
-				<Menu defaultIndex="2">
-					<MenuItem>link</MenuItem>
-					<SubMenu title="submenu">
-						<MenuItem>link</MenuItem>
-						<MenuItem>link</MenuItem>
-						<MenuItem>link</MenuItem>
-					</SubMenu>
-					<MenuItem index={`5`} disabled>
-						link-5
-					</MenuItem>
-				</Menu>
-				<p>垂直</p>
-				<Menu defaultIndex="0" mode="vertical" defaultOpenSubMenus={['1']}>
-					<SubMenu title="submenu">
-						<MenuItem>link</MenuItem>
-						<MenuItem>link</MenuItem>
-						<MenuItem>link</MenuItem>
-					</SubMenu>
-					<SubMenu title="submenu">
-						<MenuItem>link2</MenuItem>
-						<MenuItem>link2</MenuItem>
-						<MenuItem>link2</MenuItem>
-					</SubMenu>
-				</Menu>
-				<h1>Button</h1>
-				<Button>button</Button>
-			</header>
-		</div>
-	)
+  const [show, setShow] = useState(false);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>图标</h1>
+        <Icon icon="coffee" theme="danger" size="5x" />
+        <Icon icon="arrow-down" theme="danger" size="5x" />
+        <h1>menu</h1>
+        <p>水平</p>
+        <Menu defaultIndex="2">
+          <MenuItem>link</MenuItem>
+          <SubMenu title="submenu">
+            <MenuItem>link</MenuItem>
+            <MenuItem>link</MenuItem>
+            <MenuItem>link</MenuItem>
+          </SubMenu>
+          <MenuItem index={`5`} disabled>
+            link-5
+          </MenuItem>
+        </Menu>
+        <p>垂直</p>
+        <Menu defaultIndex="0" mode="vertical" defaultOpenSubMenus={['1']}>
+          <SubMenu title="submenu">
+            <MenuItem>link</MenuItem>
+            <MenuItem>link</MenuItem>
+            <MenuItem>link</MenuItem>
+          </SubMenu>
+          <SubMenu title="submenu">
+            <MenuItem>link2</MenuItem>
+            <MenuItem>link2</MenuItem>
+            <MenuItem>link2</MenuItem>
+          </SubMenu>
+        </Menu>
+        <h1>Button</h1>
+        <Button
+          size="lg"
+          onClick={() => {
+            setShow(!show);
+          }}
+        >
+          Toggle
+        </Button>
+        <Transition in={show} timeout={300} animation="zoom-in-top">
+          <div>hahaha show</div>
+        </Transition>
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,56 +1,48 @@
-import React, { FC } from 'react'
-import classNames from 'classnames'
+import React, { FC } from 'react';
+import classNames from 'classnames';
 
-export enum ButtonSize {
-	Large = 'lg',
-	Small = 'sm',
-}
-
-export enum ButtonType {
-	Primary = 'primary',
-	Default = 'default',
-	Danger = 'danger',
-	Link = 'link',
-}
+//字符串字面量
+export type ButtonSize = 'lg' | 'sm';
+export type ButtonType = 'primary' | 'default' | 'danger' | 'link';
 
 interface BaseButtonProps {
-	className?: string
-	disabled?: boolean
-	size?: ButtonSize
-	btnType?: ButtonType
-	children: React.ReactNode
-	href?: string
+  className?: string;
+  disabled?: boolean;
+  size?: ButtonSize;
+  btnType?: ButtonType;
+  children: React.ReactNode;
+  href?: string;
 }
 //& 混合两种的属性
-type NativeButtonProps = BaseButtonProps & React.ButtonHTMLAttributes<HTMLElement>
-type AnchorButtonProps = BaseButtonProps & React.AnchorHTMLAttributes<HTMLElement>
+type NativeButtonProps = BaseButtonProps & React.ButtonHTMLAttributes<HTMLElement>;
+type AnchorButtonProps = BaseButtonProps & React.AnchorHTMLAttributes<HTMLElement>;
 
 //Partial 设置属性可选
-export type ButtonProps = Partial<AnchorButtonProps & NativeButtonProps>
+export type ButtonProps = Partial<AnchorButtonProps & NativeButtonProps>;
 
 const Button: FC<ButtonProps> = (props) => {
-	const { btnType, className, disabled, size, children, href, ...restProps } = props
-	const classes = classNames('btn', className, {
-		[`btn-${btnType}`]: btnType,
-		[`btn-${size}`]: size,
-		disable: btnType === ButtonType.Link && disabled,
-	})
-	if (btnType === ButtonType.Link && href) {
-		return (
-			<a className={classes} href={href} {...restProps}>
-				{children}
-			</a>
-		)
-	}
-	return (
-		<button className={classes} disabled={disabled} {...restProps}>
-			{children}
-		</button>
-	)
-}
+  const { btnType, className, disabled, size, children, href, ...restProps } = props;
+  const classes = classNames('btn', className, {
+    [`btn-${btnType}`]: btnType,
+    [`btn-${size}`]: size,
+    disable: btnType === 'link' && disabled,
+  });
+  if (btnType === 'link' && href) {
+    return (
+      <a className={classes} href={href} {...restProps}>
+        {children}
+      </a>
+    );
+  }
+  return (
+    <button className={classes} disabled={disabled} {...restProps}>
+      {children}
+    </button>
+  );
+};
 Button.defaultProps = {
-	disabled: false,
-	btnType: ButtonType.Default,
-}
+  disabled: false,
+  btnType: 'default',
+};
 
-export default Button
+export default Button;
