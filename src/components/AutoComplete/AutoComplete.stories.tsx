7ef069bd86e397056a4bfeb63b0960c50253e87d
ziazan <494074947@qq.com>
2021-04-11 16:50:08 +0800

    update:支持自定义itemitem

--- src/components/AutoComplete/AutoComplete.stories.tsx
+++ src/components/AutoComplete/AutoComplete.stories.tsx
@@ -1,6 +1,8 @@
 import React, { PureComponent } from 'react';
 import { Story, Meta } from '@storybook/react';
 import AutoComplete, { AutoCompleteProps } from './autoComplete';
+import Icon from './../Icon';
+
 export default {
   title: 'Components/AutoComplete',
   component: AutoComplete,
@@ -19,10 +21,23 @@ const fetchSuggestions = (keyword: string) => {
 const onSelect = (item: string) => {
   console.log('item', item);
 };
+
+const renderOption = (query: string) => {
+  return (
+    <>
+      <Icon icon="book" />
+      {query}
+    </>
+  );
+};
 const DefaultAutonCompleteProps: AutoCompleteProps = {
   fetchSuggestions: fetchSuggestions,
   onSelect: onSelect,
 };
 export const Default: Story<AutoCompleteProps> = (args) => {
   return <AutoComplete {...DefaultAutonCompleteProps} />;
+};
+
+export const CustomAutoCompleteItem: Story<AutoCompleteProps> = (args) => {
+  return <AutoComplete {...DefaultAutonCompleteProps} renderOption={renderOption} />;
 };
--- src/components/AutoComplete/autoComplete.tsx
+++ src/components/AutoComplete/autoComplete.tsx
@@ -1,4 +1,4 @@
-import React, { FC, InputHTMLAttributes, ChangeEvent, useState } from 'react';
+import React, { FC, ChangeEvent, useState, ReactElement } from 'react';
 import classNames from 'classnames';
 import Input, { InputProps } from '../Input/input';
 import Icon from './../Icon';
@@ -6,6 +6,7 @@ import Icon from './../Icon';
 export interface AutoCompleteProps extends Omit<InputProps, 'onSelect'> {
   fetchSuggestions: (keyword: string) => string[];
   onSelect?: (item: string) => void;
+  renderOption?: (item: string) => ReactElement;
 }
 //可以优化的点
 //可以自定义option的样式
@@ -14,11 +15,12 @@ export interface AutoCompleteProps extends Omit<InputProps, 'onSelect'> {
 //点击外部收起菜单
 
 export const AutoComplete: FC<AutoCompleteProps> = (props) => {
-  const { fetchSuggestions, onSelect, value, className, ...restProps } = props;
+  const { fetchSuggestions, onSelect, value, className, renderOption, ...restProps } = props;
+
   const classes = classNames('jigsaw-auto-complete', className, {});
+
   const [inputValue, setInputValue] = useState(value);
   const [suggestions, setSuggestions] = useState<string[]>([]);
-  const [active, setActive] = useState(-1);
 
   /**
    * 输入值改变
@@ -36,40 +38,41 @@ export const AutoComplete: FC<AutoCompleteProps> = (props) => {
   };
 
   const handleItemClick = (item: { data?: string; index: number }) => {
-    setActive(item.index);
+    setInputValue(item.data);
+    setSuggestions([]);
     if (onSelect) {
       const _onSelect = onSelect as (item: string) => void;
       _onSelect(item.data as string);
     }
   };
-
-  const liClasses = (index: number) => {
-    return classNames('auto-complete-item', {
-      'is-active': active === index,
-    });
+  /** 渲染自定义的模板 */
+  const renderTemplate = (item: string) => {
+    return renderOption ? renderOption(item) : item;
+  };
+  const generateDropDown = () => {
+    return (
+      <ul className="jigsaw-auto-complete-list">
+        {suggestions.map((item, index) => {
+          return (
+            <li
+              className="auto-complete-item"
+              key={index}
+              onClick={(e) => {
+                handleItemClick({ data: item, index });
+              }}
+            >
+              {renderTemplate(item)}
+            </li>
+          );
+        })}
+      </ul>
+    );
   };
 
   return (
     <div className={classes}>
       <Input placeholder="请输入关键词" value={inputValue} onChange={handleChange} {...restProps} />
-      {suggestions.length > 0 && (
-        <ul className="jigsaw-auto-complete-list">
-          {suggestions.map((_str, index) => {
-            return (
-              <li
-                className={liClasses(index)}
-                key={index}
-                onClick={(e) => {
-                  handleItemClick({ data: _str, index });
-                }}
-              >
-                {_str}
-                {active === index && <Icon className="item-icon" icon="check" />}
-              </li>
-            );
-          })}
-        </ul>
-      )}
+      {suggestions.length > 0 && generateDropDown()}
     </div>
   );
 };
